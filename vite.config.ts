979d7015a929d8590aba99d61d17a7e1b279fda0
Özgür Doğan Yıldız <ozgurdoganyildiz@Ozgurs-MacBook-Air.local>
2025-01-08 18:48:03 +0300

    fix render console issue by cursor

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx']
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  }
 });
