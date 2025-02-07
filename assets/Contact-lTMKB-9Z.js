6d7fee280844917f4b0ebd17a9f9232a1d1ae8da
Özgür Doğan Yıldız <ozgurdoganyildiz@Ozgurs-MacBook-Air.local>
2025-02-07 16:42:21 +0300

    Updates

--- assets/Contact-lTMKB-9Z.js
+++ assets/Contact-lTMKB-9Z.js
@@ -1,4 +1,4 @@
-import{c as a,u as c,j as e}from"./index-Ct4jtFXp.js";/**
+import{c as a,u as c,j as e}from"./index-CcgtVwKo.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
