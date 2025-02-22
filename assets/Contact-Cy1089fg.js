f0e5362b7f00597119b1de242d716376a32c73cb
odyildiz <ozgurdoganyildiz@hotmail..com>
2025-02-22 10:56:32 +0300

    Updates

--- assets/Contact-Cy1089fg.js
+++ assets/Contact-Cy1089fg.js
@@ -1,4 +1,4 @@
-import{c as t,u as r,j as e}from"./index-B7tS6FGv.js";/**
+import{c as t,u as r,j as e}from"./index-Bvon9Esv.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
