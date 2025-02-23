58da797032908005e93a0762d74f8dbfd86ec165
odyildiz <ozgurdoganyildiz@hotmail..com>
2025-02-23 12:12:02 +0000

    Updates

--- assets/Contact-Y0869u5w.js
+++ assets/Contact-Y0869u5w.js
@@ -1,4 +1,4 @@
-import{c as t,u as r,j as e}from"./index-BXL-A5q9.js";/**
+import{c as t,u as r,j as e}from"./index-D_em9n_T.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
