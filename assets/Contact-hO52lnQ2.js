19f89decf3b6241159be0f078f5d53f3d7cc34df
odyildiz <ozgurdoganyildiz@hotmail.com>
2025-03-04 18:57:47 +0300

    Updates

--- assets/Contact-hO52lnQ2.js
+++ assets/Contact-hO52lnQ2.js
@@ -1,4 +1,4 @@
-import{c as t,u as r,j as e}from"./index-BbLJilR0.js";/**
+import{c as t,u as r,j as e}from"./index-C5CmmTf3.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
