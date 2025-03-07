8bbb42b9b3bea40a0431d83c72396b63edeb729a
odyildiz <ozgurdoganyildiz@hotmail.com>
2025-03-07 15:20:03 +0300

    Updates

--- assets/Contact-_ycN-Szf.js
+++ assets/Contact-_ycN-Szf.js
@@ -1,4 +1,4 @@
-import{c as a,u as s,j as e}from"./index-CoFNwP-d.js";/**
+import{c as a,u as s,j as e}from"./index-D2IM6ZdY.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
