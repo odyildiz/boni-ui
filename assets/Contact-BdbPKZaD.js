e57abc2a7c0724c2c3fb51b4e906a42dafc94ef2
odyildiz <ozgurdoganyildiz@hotmail.com>
2025-03-08 14:24:40 +0300

    Updates

--- assets/Contact-BdbPKZaD.js
+++ assets/Contact-BdbPKZaD.js
@@ -1,4 +1,4 @@
-import{c as a,u as s,j as e}from"./index-D2IM6ZdY.js";/**
+import{c as a,u as s,j as e}from"./index-DAoXljIe.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
